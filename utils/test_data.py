3c9ebbed44f3f91f9f34b6910900531193866d36
Andrii-Bezkrovnyi <bezkrovnyyav+1@gmail.com>
2025-12-15 11:19:32 +0200

    added test data

---
+++ utils/test_data.py
@@ -0,0 +1,30 @@
+base_url = "https://automationintesting.online"
+api_url = f"{base_url}/booking"
+
+admin_credentials = {
+    "username": "admin",
+    "password": "password"
+}
+
+valid_booking_data = {
+    "firstname": "Andrii",
+    "lastname": "Test",
+    "email": "andrii@example.com",
+    "phone": "09718618291"
+}
+
+invalid_booking_data = {
+    "firstname": "",
+    "lastname": "",
+    "email": "non-an-email",
+    "phone": "qwety"
+  }
+
+room_data = {
+    "roomName": "Test Room",
+    "type": "Single",
+    "accessible": True,
+    "description": "Test room for testing purposes",
+    "features": ["WiFi", "TV", "Safe"],
+    "roomPrice": 100
+}
